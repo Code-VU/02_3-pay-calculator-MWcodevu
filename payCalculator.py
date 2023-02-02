e33ecebafd98f1e726c72bb9f5d8ff7a90ec2e2c
Matt Wyatt <matt.wyatt@veteransunited.com>
2023-02-02 16:45:56 -0600

    fixed with floats

--- payCalculator.py
+++ payCalculator.py
@@ -4,7 +4,7 @@ def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
     rate = input("Enter Rate:")
-    pay = int(hrs)*int(rate)
+    pay = float(hrs)*float(rate)
     print(pay)
     # end assignment
 
